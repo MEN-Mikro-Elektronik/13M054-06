a02c0e45e6ae204cd15ee96dd35510025908a63c
KonradJPE <ktomasik@jpembedded.eu>
2019-05-02 11:55:01 +0200

    Removed in-file history. Added copyright and licence
    Issue 'add internal tools'
    https://github.com/MEN-Mikro-Elektronik/13M054-06/pull/1

--- DRIVERS/MDIS_LL/M054/TEST/M54_TEST/COM/m54_test.c
+++ DRIVERS/MDIS_LL/M054/TEST/M54_TEST/COM/m54_test.c
@@ -33,25 +33,24 @@
  *          libraries: mdis_api, m54_api
  *     Switches: -
  *
- *-------------------------------[ History ]---------------------------------
- *
- * $Log: m54_test.c,v $
- * Revision 1.4  2015/02/18 16:20:15  MRoth
- * R: not compatible to MDIS5 (no 64bit support)
- * M: ported to MDIS5 according porting guide rev. 0.10
- *
- * Revision 1.3  2001/09/28 08:49:16  kp
- * cosmetics
- *
- * Revision 1.2  2001/09/26 10:40:16  kp
- * added test #4 to test polarity inversion
- *
- * Revision 1.1  2001/09/25 16:27:49  kp
- * Initial Revision
- *
  *---------------------------------------------------------------------------
- * (c) Copyright 2001 by MEN mikro elektronik GmbH, Nuernberg, Germany 
+ * Copyright (c) 2001-2019, MEN Mikro Elektronik GmbH
  ****************************************************************************/
+/*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 static const char RCSid[]="$Id: m54_test.c,v 1.4 2015/02/18 16:20:15 MRoth Exp $";
 
 #include <stdio.h>
